f5f55ab3f67ae125cdb5c6b79e68fd56649c54f3
DallasLind <mcgraw.dallas@gmail.com>
2019-10-21 11:41:31 -0400

    Set up all possible winning combinations

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 console.log("Hello game!");
 
 let playerOne = 1;
+const reset = document.querySelector("button");
 let board = document.getElementById("board");
 board.addEventListener("click", boardClick);
+const squares = document.querySelectorAll(".square");
 
 const sq1 = document.querySelector("#square1");
 const sq2 = document.querySelector("#square2");
@@ -13,3 +15,32 @@ const sq6 = document.querySelector("#square6");
 const sq7 = document.querySelector("#square7");
 const sq8 = document.querySelector("#square8");
 const sq9 = document.querySelector("#square9");
+
+let winningMoves = [
+
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 3, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [7, 5, 3]
+
+];
+
+
+
+
+
+for(let i = 0; i<squares.length; i++){
+	squares[i].addEventListener("click", playerMove);
+
+	function playerMove(evt){
+		evt.preventDefault();
+		if(playerOne === 1){
+
+		}
+	}
+
+
