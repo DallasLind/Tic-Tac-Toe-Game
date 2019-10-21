6bf472c1d1c79575c16645195167fa0b6acc597c
DallasLind <mcgraw.dallas@gmail.com>
2019-10-21 11:19:35 -0400

    Defined all squares

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 console.log("Hello game!");
 
-let gameplay = 
+let playerOne = 1;
+let board = document.getElementById("board");
+board.addEventListener("click", boardClick);
+
+const sq1 = document.querySelector("#square1");
+const sq2 = document.querySelector("#square2");
+const sq3 = document.querySelector("#square3");
+const sq4 = document.querySelector("#square4");
+const sq5 = document.querySelector("#square5");
+const sq6 = document.querySelector("#square6");
+const sq7 = document.querySelector("#square7");
+const sq8 = document.querySelector("#square8");
+const sq9 = document.querySelector("#square9");
